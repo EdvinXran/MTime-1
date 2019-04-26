034943d545e38d076b004d050106457de374b45f
Gavin <zjw0702@gmail.com>
2019-04-26 09:47:31 +0800

    "逆向工程生成订单模块"

---
+++ guns-order/src/main/java/com/stylefeng/guns/rest/persistence/dao/MtimeOrderTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.persistence.dao;
+
+import com.stylefeng.guns.rest.persistence.model.MtimeOrderT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * ??????Ϣ? Mapper 接口
+ * </p>
+ *
+ * @author stylefeng
+ * @since 2019-04-25
+ */
+public interface MtimeOrderTMapper extends BaseMapper<MtimeOrderT> {
+
+}
